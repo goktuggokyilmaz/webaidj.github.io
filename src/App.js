63e59977cf92fd101effc93dc75474a992ce83ba
goktuggokyilmaz <ggokyilmaz@gmail.com>
2024-10-09 13:19:59 +0300

    Router name changed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/webaidj.github.io">
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/signin" element={<SignInPage />} />
